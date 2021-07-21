24248310601f8e95662986b6760b6c9d7dc2afbd
Filipe de Lacerda <filipew.dev@gmail.com>
2021-07-21 12:03:29 -0300

    Refatora e implementa componentes estilizados com troca de temas

---
+++ src/Components/Extrato/index.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Box, Button } from "../UI";
+import { extratoLista } from "../../info";
+import Items from "../Items";
+
+export const Extrato = () => {
+  return <Box>
+    {
+      extratoLista.updates.map(({id, type, from, value, date}) => {
+        return (
+          <Items key={id} type={type} from={from} value={value} date={date} />
+        )
+      })
+    }
+    <Button>Ver Mais</Button>
+  </Box>;
+};
